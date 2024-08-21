8c1f395c28de3fb303e6c73dd4f55762232196d1
Valenojeda <valenojeda94@gmail.com>
2024-08-21 17:08:25 -0300

    json products

--- js/products.js
+++ js/products.js
@@ -1,44 +1,3 @@
-//dirección para obtener el listado en formato json:
-const LIST_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
-
-
-//función para mostrar el spinner de carga:
-function showSpinner(){
-  document.getElementById("spinner-wrapper").style.display = "block";
-}
-
-
-//función para ocultar el spinner de carga:
-function hideSpinner(){
-  document.getElementById("spinner-wrapper").style.display = "none";
-}
-
-
-//función que realiza el fetch() a la url recibida y devuelve un objeto con los datos y el estado de la respuesta:
-function getJSONData(url){
-    let result = {};
-    showSpinner();
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
-    });
-}
 //array donde se cargarán los datos recibidos:
 let categoriesArray = [];
 
@@ -54,7 +13,7 @@ function showCategoriesList(array) {
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="${category.imgSrc}" alt="product image" class="img-thumbnail">
+                    <img src="${category.image}" alt="product image" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
@@ -62,7 +21,7 @@ function showCategoriesList(array) {
                             <h4>${category.name}</h4>
                             <p>${category.description}</p>
                         </div>
-                        <small class="text-muted">${category.productCount} artículos</small>
+                        <small class="text-muted">${category.soldCount} artículos</small>
                     </div>
                 </div>
             </div>
@@ -71,7 +30,7 @@ function showCategoriesList(array) {
 
 
     // Asignar el HTML generado al contenedor después de construirlo completamente
-    document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    document.getElementById("productsList").innerHTML = htmlContentToAppend;
 }
 
 
@@ -88,9 +47,9 @@ EJECUCIÓN:
 
 
 document.addEventListener("DOMContentLoaded", function(e) {
-    getJSONData(LIST_URL).then(function(resultObj) {
+    getJSONData(CATEGORY_AUTOS).then(function(resultObj) {
         if (resultObj.status === "ok") {
-            categoriesArray = resultObj.data;
+            categoriesArray = resultObj.data.products;
             showCategoriesList(categoriesArray); // Corrección de la variable
         }
     });
