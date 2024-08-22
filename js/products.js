d5109f2cc4cd0edbb50820b3596899521be5e0a1
cardozodaniela <danielacard89@hotmail.com>
2024-08-21 22:04:24 -0300

    Agregado de fila horizontal de a 3 elementos.

--- js/products.js
+++ js/products.js
@@ -6,28 +6,40 @@ let categoriesArray = [];
 function showCategoriesList(array) {
     let htmlContentToAppend = "";
 
+    htmlContentToAppend += '<div class="row d-flex">'; // Comienza una fila
 
     for (let i = 0; i < array.length; i++) {
         let category = array[i];
+        
         htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="${category.image}" alt="product image" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                            <h4>${category.name}</h4>
-                            <p>${category.description}</p>
-                            <p>$${category.cost}</p>
+        <div class="col-4 d-flex">
+            <div class="list-group-item list-group-item-action d-flex mb-4">
+                <div class="row">
+                    <div class="col-12">
+                        <img src="${category.image}" alt="product image" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <div class="mb-1">
+                                <h4>${category.name}</h4>
+                                <p>${category.description}</p>
+                                <p>USD ${category.cost}</p>
+                                <p>Cantidad vendidas: ${category.soldCount}</p>
+                            </div>
+                            
                         </div>
-                        <small class="text-muted">${category.soldCount} artículos</small>
                     </div>
                 </div>
             </div>
         </div>`;
+
+    
+        if ((i + 1) % 3 === 0) {
+            htmlContentToAppend += '</div><div class="row d-flex">';
+        }
     }
+
+htmlContentToAppend += '</div>';
 
 
     // Asignar el HTML generado al contenedor después de construirlo completamente
