76cac1d8152e56e1768a755b2de6cf306475a5a4
Daniela Cardozo <danielacar89@hotmail.com>
2024-10-19 17:16:56 -0300

    arreglos en responsive para ajustar modo oscuro

--- js/sell.js
+++ js/sell.js
@@ -147,3 +147,13 @@ document.addEventListener("DOMContentLoaded", function(e){
         }
     });
 });
+
+// Modo Oscuro
+const theme = localStorage.getItem('theme');
+
+// Aplicar el tema guardado al cargar la página
+if (theme === 'dark-mode') {
+    document.body.classList.add('dark-mode');
+} else {
+    document.body.classList.remove('dark-mode');
+}
