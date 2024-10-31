8e9c72dc26f277f8d0559b5fff4d688094f6a88e
Sofia Mello <sofi.mello124@gmail.com>
2024-10-31 19:52:11 -0300

    Mostrando los productos en el carrito

--- js/product-info.js
+++ js/product-info.js
@@ -99,29 +99,46 @@ document.addEventListener("DOMContentLoaded", function() {
     function addToCart(product) {
         const cart = JSON.parse(localStorage.getItem("cart")) || [];
         const productData = {
+            id: product.id,
             name: product.name,
             cost: product.cost,
             currency: product.currency,
             quantity: 1,
             image: product.images[0] // Primera imagen
         };
+
+        // Verifica si el producto ya está en el carrito
+        const existingProductIndex = cart.findIndex(item => item.id === productData.id); // Busca el índice del producto
+
+        if (existingProductIndex !== -1) {
+        // Si ya existe, incrementa la cantidad
+        cart[existingProductIndex].quantity += productData.quantity; // Aumenta la cantidad
+        } else {
+        // Si no existe, lo agrega al carrito
         cart.push(productData);
-        localStorage.setItem("cart", JSON.stringify(cart));
-        alert("Producto agregado al carrito");
     }
+
+    // Actualiza el carrito en localStorage
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert("Producto agregado al carrito");
+}
+
 
     // Función para comprar ahora
     function buyNow(product) {
         const purchaseData = {
+            id: product.id,
             name: product.name,
             cost: product.cost,
             currency: product.currency,
             quantity: 1,
             image: product.images[0] // Primera imagen
         };
+        
         localStorage.setItem("purchase", JSON.stringify([purchaseData])); // Guarda el producto actual
         window.location.href = "cart.html"; // Navega a la página de carrito
     }
+
 
     // Función para mostrar productos relacionados
     function showRelatedProducts(relatedProducts) {
